23e80273ddd8f649c861c7d4b532d7ca3226424f
Markus Obsil <markus.obsil@dynatrace.com>
2023-11-27 12:35:27 +0100

    add linting recommendations

--- scripts/check_istiod.sh
+++ scripts/check_istiod.sh
@@ -2,7 +2,7 @@
 
 ISTIO_CP_NS=$1
 
-for pod in $(kubectl get pods -n $ISTIO_CP_NS --selector=app=istiod -o custom-columns=name:metadata.name --no-headers)
+for pod in $(kubectl get pods -n "${ISTIO_CP_NS}" --selector=app=istiod -o custom-columns=name:metadata.name --no-headers)
 do
-  kubectl wait --for=condition=ready --namespace ${ISTIO_CP_NS} pod ${pod}
+  kubectl wait --for=condition=ready --namespace "${ISTIO_CP_NS}" pod "${pod}"
 done
